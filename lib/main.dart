ec8d81a1c40170da31957e2fb60d5468ec2b8a58
guilherme <guiabucarma@gmail.com>
2023-07-03 18:04:36 -0400

    add o update do projeto 03/07/23

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -43,34 +42,57 @@ class _MyHomePageState extends State<MyHomePage> {
       'userId': 1,
     };
 
+    // ######################### Dio #########################
+
+    final dio = Dio();
+
+    try {
+      Response response = await dio.post(
+        'https://jsonplaceholder.typicode.com/posts',
+        options: Options(
+          headers: headers,
+        ),
+        data: body,
+      );
+
+      //throw "Erro customizado";
+      print(response.data);
+    } on DioException catch (e) {
+      print("Erro na request: ${e.response?.statusCode}");
+
+      //print("Erro generico: $e");
+    }
+
+    // ######################### http: ^0.13.4 #########################
+
     //buscando link no tipo json com get
-    // http.Response response = await http.get(
+    // http.Response response = await http.get( http: ^0.13.4
     //   Uri.parse("https://jsonplaceholder.typicode.com/posts"),
     //   headers: headers,
     // );
 
     //buscando link no tipo json com post
-    http.Response response = await http.post(
-      Uri.parse("https://jsonplaceholder.typicode.com/posts"),
-      headers: headers,
-      body: json.encode(body),
-    );
+    // http.Response response = await http.post( http: ^0.13.4
+    //   Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+    //   headers: headers,
+    //   body: json.encode(body),
+    // );
 
     // Respostas de sucesso (200-299)
 
     //verificando resposta que servidor for 200
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      print(response.body);
-      //List result = json.decode(response.body);
+    //if (response.statusCode >= 200 && response.statusCode < 300) {  http: ^0.13.4
+    //print(response.body);
+    //List result = json.decode(response.body);  http: ^0.13.4
 
-      //buscando list json
-      // for (var item in result) {
-      //   print(item['title']);
-      // }
+    //buscando list json
+    // for (var item in result) {
+    //   print(item['title']);
+    // }
 
-    } else {
-      print('aconteceu um erro: ${response.statusCode}');
-    }
+    //} else {
+    //print('aconteceu um erro: ${response.statusCode}');  http: ^0.13.4
+    //}
   }
 
   @override
